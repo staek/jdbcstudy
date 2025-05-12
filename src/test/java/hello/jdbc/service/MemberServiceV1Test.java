7281f22113700310515ea2e5d0aae9135cf3554d
sangtaek <staek1533@gmail.com>
2025-05-12 22:56:02 +0900

    트랜잭션 스터디 1

---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,14 @@
+package hello.jdbc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceV1Test {
+
+
+
+    @Test
+    void accountTransfer() {
+    }
+}
